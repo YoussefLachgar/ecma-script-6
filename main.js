a7a9d0c34da52ed540f8a76d811fc1ff18d19425
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-03-18 08:56:34 +0000

    ex: get file extension

--- main.js
+++ main.js
@@ -28,9 +28,6 @@ const
 
 */ 
 
-const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a+b) === 100;
+const fileExtension = (fileName) => fileName.slice(fileName.lastIndexOf('.'));
 
-console.log(isEqualTo100(0,100));
-console.log(isEqualTo100(100,0));
-console.log(isEqualTo100(80,20));
-console.log(isEqualTo100(50,50));
+console.log(fileExtension("index.get.js"));
