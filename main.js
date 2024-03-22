557ba0d123471ff47836ff283886ac84df81dcd5
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-03-22 18:35:57 +0000

    ex: return the first half of a String

--- main.js
+++ main.js
@@ -28,6 +28,6 @@ const
 
 */ 
 
-const makeNewString = (str) =>  str.length < 3 ? str : str.slice(0,3) + str.slice(-3);
+const firstHalfString = (str) =>   str.slice(0,str.length / 2);
 
-console.log(makeNewString('shanthan'));
+console.log(firstHalfString('shantha'));
