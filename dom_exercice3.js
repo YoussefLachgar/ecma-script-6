863f7fa35915ec39653f99309f447ed508a75047
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-02 22:15:53 +0000

    exercice 3: add toggle / radio box for annual and monthly payments for the basic and pro plans

--- dom_exercice3.js
+++ dom_exercice3.js
@@ -4,20 +4,19 @@
   A new feature has been added to the Pro plan: '24/7 Phone support'.
   Add this using JavaScript to the list features of the Pro plan.
 */
-document.querySelector('#pro-plan ul')
-    .insertAdjacentHTML('beforeend', '<li>24/7 Phone Suport</li>');
+document
+  .querySelector("#pro-plan ul")
+  .insertAdjacentHTML("beforeend", "<li>24/7 Phone Suport</li>");
 
 /* 
   Exercise 02
   -----------
   To make the ordering of the plans more logical, using JavaScript, move the basic plan to be before (to the left) of the pro plan.
 */
-const proPlanDiv = document.querySelector('#pro-plan');
-const basicPlanDiv = document.querySelector('#basic-plan');
+const proPlanDiv = document.querySelector("#pro-plan");
+const basicPlanDiv = document.querySelector("#basic-plan");
 const parent = basicPlanDiv.parentNode;
 parent.insertBefore(basicPlanDiv, proPlanDiv);
-
-
 
 /* 
   Exercise 03
@@ -25,10 +24,9 @@ parent.insertBefore(basicPlanDiv, proPlanDiv);
   To make the Pro plan have a stronger call to action, update the current 'Get started' button to be blue (#007bff) with white text and have the text 'Buy Now' 
 */
 
-const basicPlanButton  = document.querySelector('#basic-plan button');
-basicPlanButton.classList.add('bg-blue-700', 'text-white');
-basicPlanButton.textContent = 'Buy Now';
-
+const basicPlanButton = document.querySelector("#basic-plan button");
+basicPlanButton.classList.add("bg-blue-700", "text-white");
+basicPlanButton.textContent = "Buy Now";
 
 /* 
   Exercise 04
@@ -36,8 +34,8 @@ basicPlanButton.textContent = 'Buy Now';
   There is a special offer on at the moment offering 50% more storage on the Basic plan and 25% more on the Pro plan.
   Update the basic and pro plan cards to reflect the new storage amount being offered. 
 */
-const proPlanLiStorage = document.querySelector('#pro-plan li span');
-const basicPlanLiStorage = document.querySelector('#basic-plan li span');
+const proPlanLiStorage = document.querySelector("#pro-plan li span");
+const basicPlanLiStorage = document.querySelector("#basic-plan li span");
 
 const extractStorageValue = (textContent) => {
   const regex = /\d+/; // Matches one or more digits
@@ -45,12 +43,13 @@ const extractStorageValue = (textContent) => {
   return match ? parseInt(match[0]) : 0; // Return 0 if no digits found
 };
 
-proPlanLiStorage.textContent = extractStorageValue(proPlanLiStorage.textContent) * 1.5;
-basicPlanLiStorage.textContent = extractStorageValue(basicPlanLiStorage.textContent) * 1.25;
+proPlanLiStorage.textContent =
+  extractStorageValue(proPlanLiStorage.textContent) * 1.5;
+basicPlanLiStorage.textContent =
+  extractStorageValue(basicPlanLiStorage.textContent) * 1.25;
 
 console.log(proPlanLiStorage);
 console.log(basicPlanLiStorage);
-
 
 /* 
   Exercise 05
@@ -58,3 +57,37 @@ console.log(basicPlanLiStorage);
   You have been asked to add ask a toggle / radio box for annual and monthly payments for the basic and pro plans.
   Add a radio button to toggle between monthly and annual payment options and adjust the prices shown for both plans if the annual option is selected (give two months free if customers pay annually).
 */
+
+document.querySelector(".container").insertAdjacentHTML(
+  "beforeBegin",
+  `
+<label>
+  Monthly
+  <input id="monthlyPricing" type="radio" name="pricing" value="monthly" checked/>
+</label>
+<label>
+  Annual
+  <input id="annualPricing" type="radio" name="pricing" value="annual"/>
+</label>
+`
+);
+
+const pricing = {
+  monthly: {
+    basic: "10 / month",
+    pro: "30 / month",
+  },
+  annual: {
+    basic: "100 / year",
+    pro: "300 / year",
+  },
+};
+
+const changePricing = (event) => {
+  const selection = event.target.value;
+  document.querySelector('#basic-plan .pricing').textContent = pricing[selection].basic;
+  document.querySelector('#pro-plan .pricing').textContent = pricing[selection].pro;
+}
+
+Array.from(document.querySelectorAll('input[type="radio"]'))
+  .forEach(radio => radio.addEventListener('change', changePricing));
