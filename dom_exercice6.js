a7f9ac93fcbde7c4f0eac504de874710e2f1208e
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-06 12:39:24 +0000

    exercice 6: set up the exercice and the design using tailwind CSS.

---
+++ dom_exercice6.js
@@ -0,0 +1,32 @@
+/*
+  Exercise 01
+  -----------
+  Add a new row to the table with the details: 'Sean Reyes' (@sreyes)
+*/
+
+/*
+  Exercise 02
+  ------------
+  Update Leona Dixon's handle to @dixonl
+*/
+
+
+/*
+  Exercise 03
+  -----------
+  Move Rosa Reed to the top of the table and ensure all the number entries in the first column are updated accordingly.
+*/
+
+
+/*
+  Exercise 04
+  -----------
+  Move the handle column including all of it's rows of contents to be the second column in the table (after the # column)
+*/
+
+
+/*
+  Exercise 05
+  -----------
+  To make the table easier to read, make every other row on the table to have a darker, shaded colour e.g. #f2f2f2
+*/
