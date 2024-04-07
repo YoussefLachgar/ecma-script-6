4793ae9ac4836a930d78c28d83075589835f24b3
YoussefLachgar <48853212+YoussefLachgar@users.noreply.github.com>
2024-04-07 14:01:20 +0000

    Update kataTraining.js by refactoring the guessBlue function to a arrow function

--- kataTraining.js
+++ kataTraining.js
@@ -10,7 +10,7 @@
 // the number of red marbles pulled out so far (always lower than the starting number of red marbles)
 // guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
 
-function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+const guessBlue = (blueStart, redStart, bluePulled, redPulled) => {
   const numOfBlue = blueStart - bluePulled;
   const numOfRed = redStart - redPulled;
   
