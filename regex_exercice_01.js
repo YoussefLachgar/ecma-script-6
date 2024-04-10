38fb838a9c36cac77589cb070a90b1160c69b0f3
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-10 17:26:58 +0000

    regular expression exercice 1: get the 3 letter words in the ex1 string

---
+++ regex_exercice_01.js
@@ -0,0 +1,37 @@
+const ex1 = 'The quick brown fox jumped over the lazy dog';
+const ex2 = 'A1B2C3D4E5F6G7H8I9J10';
+const ex3 = 'The salad costs $9.99';
+const ex4 = 'Contact customer support on 0800 300 500';
+const ex5 = 'You can contact me on Twitter @codebubb or james@juniordevelopercentral.com';
+
+// Exercise 01
+// Using a regex pattern, get the 3 letter words in the ex1 string.
+
+console.log(ex1.match(/(^|\s)[a-z]{3}($|\s)/ig));
+
+
+
+// Exercise 02
+// Using a regex pattern, remove all of the numbers from the ex2 string.
+
+
+
+
+
+// Exercise 03
+// Using a regex pattern, find the monetary value contained within the ex3 string.
+
+
+
+
+
+// Exercise 04
+// Using a regex pattern, find the telephone number contained within the ex4 string.
+
+
+
+
+
+
+// Exercise 05
+// Using a regex pattern, find the email address contained within the ex5 string.
