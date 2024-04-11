bf89b7ce1edbf71cf9a8cc98b44743346ff69ae3
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-11 15:56:14 +0000

    regular expression exercice 2: get an array of numbers that are contained in the string.

--- regex_exercice_02.js
+++ regex_exercice_02.js
@@ -1,6 +1,6 @@
 const ex1 = 'Grade 1Grade 2Grade 3Grade 4Grade 5';
 const ex2 = 'The pheasant has no agenda';
-const ex3 = '75, 50 : 123 / 900 - 321 + 900 = 55';
+const ex3 = '75, 50 : 123 / 900 - 321 + 900 = 55 44b4';
 const ex4 = 'My name is: James';
 const ex5 = `Did you find any droids? No, sir. If there were any on board, they must also have jettisoned. Send a scanning crew on board. I want every part of this ship checked. Yes, sir. I sense something...a presence I haven't felt since... Get me a scanning crew in here on the double. I want every part of this ship checked! Boy, it's lucky you've these compartments. I use them for smuggling. I never thought I'd be smuggling myself in them. This is ridiculous. Even if I could take off, I'd never get past the tractor beam.`
 
@@ -16,14 +16,14 @@ const sp = ex1.split(/(Grade\s\d)/g).filter(Boolean);
 
 
 
-console.log(ex2.match(/(agenda|age)/g) ? true : false);
-console.log(/age[nda]?/.test(ex2));
+// console.log(ex2.match(/(agenda|age)/g) ? true : false);
+// console.log(/age[nda]?/.test(ex2));
 
 
 // Exercise 03
 // Using a regex, get an array of numbers that are contained in the string (removing any symbols)
 
-
+console.log(ex3.match(/\d+/g));
 
 
 
